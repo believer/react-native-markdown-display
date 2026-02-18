5396c0f1bc0511cf65afb77305b299cd07e4552d
Natt och Dag, Rickard <rickard.natt-och-dag@seb.se>
2026-02-18 10:24:55 +0100

    fix: use aspect ratio for images

--- src/lib/components/Image.js
+++ src/lib/components/Image.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Image as RNImage } from 'react-native'
 
 export function Image(props) {
-	const [layout, setLayout] = React.useState(0)
 	const [size, setSize] = React.useState({ height: 0, width: 0 })
 
 	React.useEffect(() => {
@@ -15,17 +14,10 @@ export function Image(props) {
 		return null
 	}
 
-	const ratio = layout / size.width
-	const height = Math.round(size.height * ratio)
-
 	return (
 		<RNImage
 			{...props}
-			onLayout={(event) => {
-				const { width } = event.nativeEvent.layout
-				setLayout(width)
-			}}
-			style={{ flexGrow: 1, height, width: layout }}
+			style={{ flexGrow: 1, aspectRatio: 16/9 }}
 		/>
 	)
 }
